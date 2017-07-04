f82060ccda0fa8e9c93ec14df713828b56261a7a
mariam <mariam@rodin.io>
2017-07-04 04:29:58 +0400

    Added test case for checking that project url in settings is not editable.

---
+++ e2e/tests/General_Settings.ts/Non_editable_project_url_check.tc.js
@@ -0,0 +1,42 @@
+/**
+ * Created by melkabelka on 7/4/17.
+ */
+
+const common = require('../utils/common');
+const globalFunc = require('../components/globalFunctions');
+const objMap = require('../components/objectMap');
+
+const EC = protractor.ExpectedConditions;
+
+describe('General_Settings.ts', () => {
+
+    beforeEach(() => {
+        common.goToUrl('login');
+    });
+
+    it('Login_with_existing_cridentals.tc', () => {
+        globalFunc.isDisplayed_Login_Fields();
+        globalFunc.processLogin(common.TESTUSERS[6].username, common.TESTUSERS[6].password, true);
+    });
+	
+    it('Create_project_with_unique_URL.tc', () => {
+
+        // create a new project with unique URL	
+    	globalFunc.createProject('Basic', 'AbC123', '123url','project description', "", false);  
+
+    });
+
+    it('Non_editable_project_url_check.tc', () => {
+
+        // create a new project with unique URL 
+        globalFunc.open_project_settings("AbC123");
+        expect(element(by.className('form-control input-blue')).getAttribute('disabled')).toBe('true');
+    });
+    
+    it('Cleanup.tc', () => 
+    {
+		  globalFunc.delete_project("AbC123",true);
+        
+    });
+
+});
