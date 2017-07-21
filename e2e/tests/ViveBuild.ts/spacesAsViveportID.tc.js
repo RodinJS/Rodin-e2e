f70baa4d974bfca0a556419249a51d0c219b4acd
mariam <mariam@rodin.io>
2017-07-22 01:56:41 +0400

    Added test case.

---
+++ e2e/tests/ViveBuild.ts/spacesAsViveportID.tc.js
@@ -0,0 +1,89 @@
+/**
+ * Created by melkabelka on 7/14/17.
+ */
+
+const common = require('../utils/common');
+const globalFunc = require('../components/globalFunctions');
+const objMap = require('../components/objectMap');
+const viveCommon = require('../ViveBuild.ts/viveCommon');
+const EC = protractor.ExpectedConditions;
+
+
+describe('ViveBuild.ts', () => {
+
+    beforeEach(() => {
+        browser.driver.manage().window().maximize();
+        common.goToUrl('login');
+    });
+
+    it('loginWithExistingCridentals.tc', () => {
+        // Login into user account
+        globalFunc.isDisplayed_Login_Fields();
+        globalFunc.processLogin(common.TESTUSERS[3].username, common.TESTUSERS[3].password, true);
+    });
+    
+    it('createProjectWithUniqueURL.tc', () => {
+
+        // create a new project for vive Build       
+        globalFunc.createProject("Basic", 'viveProj', 'viveurl','vive project description', "", true);
+
+    });
+
+    it('spacesAsViveportID.tc', () => {
+        
+        // go to created viveProj project's settings
+        globalFunc.open_project_settings("viveProj");
+
+        // click on vive tab
+        objMap.viveTab.click();
+
+        // swipe viveport trigger
+        viveCommon.viveTrigger.click();
+
+        // wait visibility of viveportID
+        expect(browser.wait(EC.visibilityOf(viveCommon.viveportID), 5000)).toBe(true);
+
+        // wait visibility of viveportKey
+        expect(browser.wait(EC.visibilityOf(viveCommon.viveportKey), 5000)).toBe(true);
+
+        // fill all vive fields
+        viveCommon.viveFieldsFill("AppTest", "1.1.1", true, ' ', "key");
+
+        let error = viveCommon.viveportID.element(by.xpath("../div[@class='validation error ng-scope']"));
+
+        // checking that error appeared that application name is not specified
+        browser.wait(EC.visibilityOf(error), 15000,'Wait for error element to appear').then (() =>
+        {
+            error.getText().then(function(text)
+           {
+               expect(text).toBe("Viveport id is required");
+           });
+
+        });
+        
+        // click on build button
+        this.submitBtn = element(by.buttonText('Build')).click();
+
+        // checking that error appeared that application name is not specified
+        browser.wait(EC.visibilityOf(error), 15000,'Wait for error element to appear').then (() =>
+        {
+            error.getText().then(function(text)
+           {
+               expect(text).toBe("Viveport id is required");
+           });
+
+       });
+
+    });
+
+    it('Cleanup.tc', () => {
+        //go to Dashboard
+        element(by.linkText('Dashboard')).click().then(() => {
+            expect(browser.getCurrentUrl()).toEqual(common.CONSTANTS.spaceURL+"dashboard");
+        });
+
+        globalFunc.delete_project("viveProj",true);
+
+    });
+
+});
