2a56609484a24c7005676c8ff0d3eebbe492d166
mariam <mariam@rodin.io>
2017-09-15 00:05:56 +0400

    Temp. I should remove sleeps

---
+++ e2e/tests/Login.ts/validUsernameForPasswordReset.tc.js
@@ -0,0 +1,208 @@
+/**
+ * Created by melkabelka on 25/7/17.
+ */
+
+const common = require('../utils/common');
+const globalFunc = require('../components/globalFunctions');
+const objMap = require('../components/objectMap');
+const EC = protractor.ExpectedConditions;
+
+describe('EditProfile.ts', () => {
+
+    beforeEach(() => {
+        common.goToUrl('login');
+    });
+
+    xit('loginWithExistingCridentals.tc', () => {
+
+        // Login into user account
+        globalFunc.isDisplayed_Login_Fields();
+        globalFunc.processLogin(common.TESTUSERS[6].username, common.TESTUSERS[6].password, true);
+
+         // go to edit profile section
+        globalFunc.openEditProfile();
+       
+        // clean previous email
+        objMap.editEmail.clear();
+
+        // change email to registered one
+        objMap.editEmail.sendKeys('rodintesting@mailinator.com');
+        
+        // click on Update profile
+        objMap.updateProfile.click();
+
+        // check that profile is updated
+        expect(objMap.notificationsArray.count()).toBe(1);
+        expect(objMap.notificationsArray.get(0).getText()).toBe('Profile Updated');
+
+        browser.sleep(5000);
+        // sign out
+        let userMenu = browser.findElement(by.id('accountLabel'));
+        userMenu.click();
+        let signOut = element(by.className('signout-link'));
+        signOut.click();
+
+         // click on forgot password link
+        objMap.forgetPswdLink.click();
+
+        // type non existing username
+        objMap.resetPassEditBox.sendKeys(common.TESTUSERS[6].username);
+        
+        // click on Submit button
+        objMap.submitBtn.click();
+
+        browser.sleep(3000);
+
+
+    });
+
+    xit('validUsernameForPasswordReset.tc', () => {
+
+        browser.restart();
+        browser.waitForAngularEnabled(false);
+        browser.get("https://mailinator.com");
+
+        browser.findElement(by.id('inboxfield')).sendKeys('rodintesting');
+        element(by.partialButtonText('Go!')).click();
+
+        //let objM = element(by.css('div[title="FROM"]'));
+        //browser.wait(EC.textToBePresentInElement(objM, 'Rodin team'), 15000);
+
+        expect(element(by.css('div[title="FROM"]')).getText()).toBe('Rodin team');
+
+        let objM = element(by.css('div[title="FROM"]'));
+        objM.click();
+        browser.sleep(7000);
+
+        //this.parM = element(by.id("msg_body"));
+        browser.switchTo().frame('msg_body');
+        browser.findElement(by.tagName('a')).click();
+        browser.sleep(3000);
+        globalFunc.browserTabChange(1);
+
+        this.passMY = element(by.name("password"));
+        this.passMY.sendKeys("a1234567");
+
+        this.confirm = element(by.name("confirm_password"));
+        this.confirm.sendKeys("a1234567");
+        this.formCon = element(by.name("changePasswordForm"));
+        this.formCon.element(by.partialButtonText("Submit")).click();
+        browser.sleep(3000);
+////////////////////////////////////////////////////////////
+        // let error = this.passMY.element(by.xpath("../div[@class='validation ng-binding error']"));
+
+        // browser.wait(EC.visibilityOf(error), 15000,'Wait for error element to appear').then (() =>
+        // {
+        //     error.getText().then(function(text)
+        //    {
+        //        expect(text).toBe("Password must contain at least 8 characters, including numbers and letters");
+        //    });
+
+        // });
+//////////////////////////////////////////////////
+        //this.parM.element(by.tagName('a')).click();
+
+        // browser.findElement(by.css('span[title="Delete Emails"]')).click();
+        // browser.sleep(5000);
+
+        // broser.actions()
+        // {
+             
+        // }
+        // browser.actions().
+        //     keyDown(Key.CONTROL).
+        //     keyDown(Key.T);
+
+        //browser.actions().sendKeys(protractor.Key.chord(protractor.Key.CONTROL, "t")).perform();
+        //browser.actions().keyDown(protractor.Key.CONTROL).sendKeys('a').perform();
+        //browser.actions().sendKeys("t").perform();
+        // globalFunc.isDisplayed_Login_Fields();
+        // browser.sleep(5000);
+        // browser.actions().keyDown(protractor.Key.CONTROL).
+        // objMap.forgetPswdLink.click();
+
+        // var elm = element(by.id('my_id'));
+
+        // browser.actions()
+        //     .mouseMove(objMap.forgetPswdLink)
+        //     .keyDown(protractor.Key.CONTROL)  // COMMAND for Mac 
+        //     .click()
+        //     .perform();
+// var hotkeys = require('protractor-hotkeys');
+        // hotkeys.trigger('ctrl+n');
+        // browser.sleep(5000);
+        
+        // // click on forgot password link
+        // objMap.forgetPswdLink.click();
+
+        // // type non existing username
+        // objMap.resetPassEditBox.sendKeys(common.TESTUSERS[6].username);
+        
+        // // click on Submit button
+        // objMap.submitBtn.click();
+
+        // // check error
+        // browser.wait(EC.textToBePresentInElement(objMap.notificationsArray.get(0).getText(), "User doesn't exist"), 5000);
+        // expect(objMap.notificationsArray.count()).toBe(1);
+    });
+    
+
+    it('changeUserPassword.tc', () => {
+
+        browser.get("login");
+
+        globalFunc.isDisplayed_Login_Fields();
+        globalFunc.processLogin(common.TESTUSERS[6].username, "a1234567", true);
+
+        //go to edit profile section
+        globalFunc.openEditProfile();
+       
+        // clean previous email
+        objMap.editEmail.clear();
+
+        // change email to registered one
+        objMap.editEmail.sendKeys('mariam@rodin.io');
+        
+        // click on Update profile
+        objMap.updateProfile.click();
+
+        // check that profile is updated
+        expect(objMap.notificationsArray.count()).toBe(1);
+        expect(objMap.notificationsArray.get(0).getText()).toBe('Profile Updated');
+       
+        // go to passwors tab
+        objMap.passwordTab.click();
+
+        //type new password in new password field
+        objMap.newPassword.sendKeys(common.TESTUSERS[6].password);
+
+        // confirm you set password
+        objMap.confirmPassword.sendKeys(common.TESTUSERS[6].password);
+
+        // click on Update button
+        objMap.confirmPass.click();
+
+        // check that password is not updated. No notification pops up.
+      //  expect(objMap.notificationsArray.count()).toBe(1);
+      //  expect(objMap.notificationsArray.get(0).getText()).toBe('Password successfully updated');
+
+
+        // sign out
+        let userMenu = browser.findElement(by.id('accountLabel'));
+        userMenu.click();
+        let signOut = element(by.className('signout-link'));
+        signOut.click();
+
+        // login
+        globalFunc.processLogin(common.TESTUSERS[6].username, "a1234567", false);
+        objMap.passwordField.clear();
+        objMap.userNameField.clear();
+        globalFunc.processLogin(common.TESTUSERS[6].username, common.TESTUSERS[6].password, true);
+
+    
+        userMenu.click();
+        signOut.click();
+
+    });
+
+});
